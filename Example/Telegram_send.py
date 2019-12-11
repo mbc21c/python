bb224da0197bae26686267323ad763813a80507c
Min Byung Chan <mbc21c@gmail.com>
2019-12-11 18:54:07 +0900

    텔레그램 민윤기 추가

--- Example/Telegram_send.py
+++ Example/Telegram_send.py
@@ -3,7 +3,7 @@ a_bot = telepot.Bot(token = '640207892:AAEbeoJ3T-MHl-_C_DJoaHzit_TK_X_RQX4')
 myk = 662355340
 mbc = 215597857
 
-a_bot.sendMessage(myk, "ㅁㅁㅁ")
+a_bot.sendMessage(myk, "아프지 말어...ㅠㅠ")
 
 #for i in a_bot.getUpdates():
 #    print(i)
